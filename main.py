a001b859109d7168443b3e050a8bd3caa4fec547
Ales Hoheger <ales.hoheger@t-2.net>
2020-10-12 18:01:12 +0200

    osnovne spremenljivka v Pythonu

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+
 x = 5
 y = 2
 
-print(x + y)
+pi = 3.14
 
+tekst = "to je nek poljuben tekst"
+krneki_spremenljivka = 2
+
+boolova_vrednost = True
+
+prazna_spremenljivka = None
+
+print(x + y)
+print(tekst)
