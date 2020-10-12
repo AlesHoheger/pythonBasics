25a5ea76e217f65dfc4dd543e79da6c549d24a2d
Ales Hoheger <ales.hoheger@t-2.net>
2020-10-12 23:14:24 +0200

    končano, ampak elif ne dela

---
+++ homework_8-2.py
@@ -0,0 +1,12 @@
+
+#naloga 8.2
+
+secret = input("Imamo nagradno vprašanje za vas. Uganite skrito število od 1 do 9: ")
+
+if secret == 5:
+    print("Čestitamo, uganili ste skrito številko!")
+elif (secret == 4) or (secret == 6):
+    print("Blizu, ampak žal niste ugotovili.")
+else:
+    print("Napačno, več sreče prihodnjič.")
+
