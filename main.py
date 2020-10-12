1e0091db8a1585a4507bb25959d542b9dbe39db1
Ales Hoheger <ales.hoheger@t-2.net>
2020-10-12 22:37:52 +0200

    finish

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 
-x = 5
-y = 2
+x = int(5)
+y = int(2)
 
-pi = 3.14
+pi = float(3.14)
 
-tekst = "to je nek poljuben tekst"
-krneki_spremenljivka = 2
+tekst = str("to je nek poljuben tekst")
 
-boolova_vrednost = True
+boolova_vrednost = bool(True)
 
 prazna_spremenljivka = None
 
-print(x + y)
-print(tekst)
+# print(tekst + str(boolova_vrednost))
+# print(tekst)
+
+# user_name = input("Vpisi svoje ime ")
+# print("Pozdravljen " + user_name + "!")
+
+#naloga:
+x = int(input("Vpiši prvo številko "))
+y = int(input("Vpiši drugo številko "))
+print("Rešitev je: " + str(x + y))
+
+print(x == 0)
+
+if (x == 0) or (y == 0):
+    print("prvo stevilo je bilo nic")
+elif x <= 0:
+    print("prvo stevilo je negativno")
+else:
+    print("prvo stevilo je pozitivno")
+
