1089a441e38cb34d16c17102db9c186c0aec769a
Ales Hoheger <ales.hoheger@t-2.net>
2020-10-12 22:54:27 +0200

    končana naloga

---
+++ homework_8-1.py
@@ -0,0 +1,12 @@
+
+#naloga 8.1
+
+mood = input("Vpiši tvoje razpoloženje: ")
+
+if mood == "vesel":
+    print("Odlično, to je lepo slišati.")
+elif mood == "nervozen":
+    print("Globoko vdihni 3-krat!")
+else:
+    print("Ne poznam tega razpoloženja :-(")
+
