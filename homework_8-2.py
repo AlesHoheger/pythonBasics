e8fee10a63b5dd2cd6367badb4150aea6747a16b
Ales Hoheger <ales.hoheger@t-2.net>
2020-10-14 23:33:12 +0200

    popravek komentarja in tip vrednosti spremenljivke

--- homework_8-2.py
+++ homework_8-2.py
@@ -1,11 +1,11 @@
 
-#naloga 8.2
+# naloga 8.2
 
 secret = input("Imamo nagradno vprašanje za vas. Uganite skrito število od 1 do 9: ")
 
-if secret == 5:
+if secret == "5":
     print("Čestitamo, uganili ste skrito številko!")
-elif (secret == 4) or (secret == 6):
+elif (secret == "4") or (secret == "6"):
     print("Blizu, ampak žal niste ugotovili.")
 else:
     print("Napačno, več sreče prihodnjič.")
