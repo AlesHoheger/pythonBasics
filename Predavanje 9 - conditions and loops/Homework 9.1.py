c6366d1f46ca354a7f1af19f01b199fbd997ea04
Ales Hoheger <ales.hoheger@t-2.net>
2020-10-19 00:10:18 +0200

    narejen kalkulator

--- Predavanje 9 - conditions and loops/Homework 9.1.py
+++ Predavanje 9 - conditions and loops/Homework 9.1.py
@@ -1,4 +1,3 @@
-
 print("Program za konvertiranje kilometrov v milje!")
 
 mile = float(0.62)
@@ -6,5 +5,6 @@ mile = float(0.62)
 km = int(input("Vpišite število kilometrov: "))
 
 print(mile * km)
+
 
 # kako bi bil pravi print ukaz, da bi izpisalo "50 km je 31.0 milj"
