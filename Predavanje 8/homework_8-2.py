7b882c2403aca911401c79767c8182dcaf65f84f
Ales Hoheger <ales.hoheger@t-2.net>
2020-10-19 18:23:17 +0200

    ne dela line 16

--- Predavanje 8/homework_8-2.py
+++ Predavanje 8/homework_8-2.py
@@ -1,17 +1,22 @@
+import random
 
-# naloga 8.2
+secret = random.randint(1, 9)
 
-secret = 5
-guess = None
-
-
-while guess != secret:
-
+poizkus = 0
+while True:
+    poizkus += 1
     guess = int(input("Imamo nagradno vprašanje za vas. Uganite skrito število od 1 do 9: "))
+    print("to je vaš " + str(poizkus) + ". poizkus")
 
     if guess == secret:
         print("Čestitamo, uganili ste skrito številko!")
+        with open("highscore.txt", "r") as scores:
+            best_score = int(scores.read())
+        if poizkus < best_score:
+            with open("highscore.txt, "a") as scores:
+                scores.write(str(poizkus))
 
+        break
     elif (guess == (secret -1)) or (guess == (secret + 1)):
         print("Blizu, ampak žal niste ugotovili.")
     else:
